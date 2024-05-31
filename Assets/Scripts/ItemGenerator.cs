523697dc562fac513be3e57e2cad14a9dea9288f
KakinumaYuzuki <V.proYuzu23@gmail.com>
2024-06-01 02:36:46 +0900

    ロジックを使った生成

---
+++ Assets/Scripts/ItemGenerator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    private GameObject _item;
+    void Start()
+    {
+        _item = (GameObject)Resources.Load("Item");
+        InstantiateItem();
+    }
+
+    private void InstantiateItem()
+    {
+        for (int i = -5; i >= -100; i -= 5)
+        {
+            Instantiate(_item, new Vector3(0, 1, i), Quaternion.identity).transform.parent = this.transform;
+        }
+    }
+}
